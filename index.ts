28f67e9465a7e37bee0ed6dde06f306f87d098ec
Shane2003s <shanehtetnaing@outlook.com>
2023-08-20 23:35:38 +0630

    testing on development branch

--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
 import fs from "fs";
-console.log("Ok this is the testing new branch");
+// console.log("Ok this is the testing new branch");
+fs.writeFile("hellothree.txt", "this is the testing content", function (err) {
+  console.log("saved");
+});
